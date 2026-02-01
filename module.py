034a50879f770058326cfe33a7af5596c6e9563a
Dashrath Patidar <patidardashrath364@gmail.com>
2026-02-01 18:29:15 +0530

    first commit

---
+++ module.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    sum = a+b
+    return sum
+print(add(23,243))
+
+def sub(a,b):
+    min = a-b
+    return min
+print(245,243)
+
+def mult(a,b):
+    all = a*b
+    return all
+print(23,243)
